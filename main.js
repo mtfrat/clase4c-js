f3f0ceaa6542ea351137035e5d9b13b3b29fee77
mtfrat <martinfraticelli@gmail.com>
2021-08-23 16:14:21 -0300

    desafio corregido

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 // Consigna: codifica al menos tres funciones cuyas instrucciones permitan resolver un problema particular, segmentado en tareas. La información a procesar debe ser ingresada por el usuario, y el resultado del procesamiento visualizado en una salida.
 
-function ingresarPrecio() {
-    valorIngresado = parseInt(prompt("Ingrese precio: "))
-    return valorIngresado
+let cantidad = 0
+
+function ingresoProductos() {
+    var decision = "n"
+    var precioTotal = 0
+    while (decision !== "y"){
+        var precioUnitario = parseInt(prompt("Ingrese precio: "))
+        precioTotal += precioUnitario
+        cantidad += 1
+        decision = prompt("Presione 'y' si finalizo de ingresar productos: \nDe no ser así, presione cualquier tecla")
+    }
+    if(decision == "y"){
+        return precioTotal
+    }
 }
 
-function mostrarPrecios(bruto,neto){
-    console.log("El precio bruto es: ", bruto)
-    console.log("El precio neto es: ", neto)
+function mostrarPrecios(bruto,neto, cant){
+    console.log("El precio bruto total es: ", bruto)
+    console.log("El precio neto total es: ", neto)
+    console.log("La cantidad de productos fueron: ", cantidad)
 }
 
-let precioBruto = ingresarPrecio()
+
+let precioBruto = ingresoProductos()
 let calcularIva = precioBruto => precioBruto * 1.21
 mostrarPrecios(precioBruto,calcularIva(precioBruto))
